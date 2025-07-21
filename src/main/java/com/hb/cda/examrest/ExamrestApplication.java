d29f3ecc09e32e30e02576a604e9a6fe6c1b1fb9
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-07-21 15:08:26 +0200

    Initial commit

---
+++ src/main/java/com/hb/cda/examrest/ExamrestApplication.java
@@ -0,0 +1,13 @@
+package com.hb.cda.examrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamrestApplication.class, args);
+	}
+
+}
