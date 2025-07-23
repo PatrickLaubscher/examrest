7b211dd0974ab3cd6d4ae075a931e5a6d7c15027
Patrick Laubscher <patricklaubscher@hotmail.com>
2025-07-23 09:06:20 +0200

    Update ContributorRepository.java

--- src/main/java/com/hb/cda/examrest/repository/ContributorRepository.java
+++ src/main/java/com/hb/cda/examrest/repository/ContributorRepository.java
@@ -1,10 +1,17 @@
 package com.hb.cda.examrest.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hb.cda.examrest.model.Contributor;
 import com.hb.cda.examrest.model.ContributorId;
 
 public interface ContributorRepository extends JpaRepository<Contributor, ContributorId> {
+
+    Optional<Contributor> findByUserEmailAndGroupNumber(String email, Integer number);
+
+    List<Contributor> findByGroupNumber(Integer number);
     
 }
